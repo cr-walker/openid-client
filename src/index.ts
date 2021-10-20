3d2ba0fc810ab071e80de5ed2b20e75d6723be6f
Chris Walker <chris@ckwalker.com>
2021-10-20 09:55:00 -0700

    testing custom claims

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ const secret = 'TQV5U29k1gHibH5bx1layBo0OSAvAbRT3UYW3EWrSYBB5swxjVfWUa1BS8lqzxG/
 const routes : any = {
   home: '/',
   profile: '/profile',
-  admin: '/admin'
+  admin: '/admin',
+  buy: '/buy',
+  blocked: '/blocked'
 }
 
 const actions : any = {
@@ -22,16 +24,17 @@ const actions : any = {
 app.use(
   auth({
     idpLogout: true,
-    // only require auth on specific endpoints
-    authRequired: false,
-    // routes: {
-    //   login: false,
-    //   postLogoutRedirect: '/custom-logout'
-    // },
+    authRequired: false, // only require auth on specific endpoints
     issuerBaseURL: 'http://localhost:3000',
     baseURL: 'http://localhost:3001',
     clientID,
-    secret
+    secret,
+    authorizationParams: {
+      scope: 'openid profile email verification' //custom 'verification' scope
+    },
+    routes: {
+      postLogoutRedirect: 'http://localhost:3001/'
+    }
   })
 )
 
@@ -42,15 +45,43 @@ const links = Object.keys(routes).map((k : string) => `<a href="${routes[k]}">${
 const buttons = Object.keys(actions).map((k : string) => `<a href="${actions[k]}"><button>${k}</button></a>`).join('<br>')
 const navbar = links.concat('<br>').concat(buttons).concat('<br>')
 
-app.get(routes.home, (req : any, res) => {
-  res.send(`${navbar}Home - no auth required - welcome ${name(req)}`)
-})
+app.get(
+  routes.home,
+  (req : any, res) => {res.send(`${navbar}Home - no auth required - welcome ${name(req)}`)}
+)
 
 // any logged in user
-app.get(routes.profile, requiresAuth(), (req : any, res) => res.send(`${navbar}Admin (must be authenticated) welcome ${req.oidc.user.sub}`))
+app.get(
+  routes.profile,
+  requiresAuth(),
+  (req : any, res) => res.send(`${navbar}Profile (must be authenticated) welcome ${req.oidc.user.sub}`)
+)
 
 // must have specific claims
-app.get(routes.admin, claimIncludes('roles', 'admin'), (req : any, res) => res.send(`${navbar}Admin - top secret, login required, welcome ${req.oidc.user.sub}`))
+app.get(
+  routes.admin,
+  claimIncludes('roles', 'admin'),
+  (req : any, res) => res.send(`${navbar}Admin (must be authenticated and admin) welcome ${req.oidc.user.sub}`)
+)
+
+// using our custom oidc claims: must have verified US address to participate in a sale
+app.get(
+  routes.buy,
+  (req: any, res, next) => {
+    const country = req.oidc.country
+    const verified = req.oidc['https://tokensoft.io/identity_verified']
+    // return new Error(JSON.stringify(req.oidc))
+    // only continue if user qualifies
+    return country == 'US' && verified ? next() : res.redirect(routes.blocked)
+  },
+  (req : any, res) => res.send(`${navbar}Sale (must be authenticated and a verified US user) you are approve to purchase X coins ${req.oidc.user.sub}`)
+)
+
+// a blocked user
+app.get(
+  routes.blocked,
+  (req : any, res) => res.send(`${navbar}Blocked! ${req.oidc.user.sub} - do you even KYC?`)
+)
 
 app.get(actions.login, (_, res: any) => res.oidc.login({ returnTo: routes.admin }))
 
